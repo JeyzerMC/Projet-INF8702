27c1a0b676434843c327c12a760aa4521b49f5df
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-11-10 22:34:47 -0500

    Add glew

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 
 int main() {
+    spdlog::set_level(spdlog::level::debug);
+
     if (!glfwInit()) {
         spdlog::error("GLFW3 initialization failed");
         return -1;
@@ -15,6 +18,39 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+
+    // We want to use OpenGl 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    auto glewInitResult = glewInit();
+    if (glewInitResult != GLEW_OK) {
+        spdlog::error("{}", glewGetErrorString(glewInitResult));
+        glfwTerminate();
+        return -1;
+    }
+    spdlog::info("Using GLEW {}", glewGetString(GLEW_VERSION));
+
+    spdlog::debug("Latest supported OpenGL version: {}", glGetString(GL_VERSION));
+    spdlog::debug("Using graphics driver: {}", glGetString(GL_RENDERER));
+
+    if (glewIsSupported("GL_VERSION_4_6")) {
+        spdlog::info("OpenGl 4.6 is supported");
+    }
+    else {
+        spdlog::error("OpenGl 4.6 is not supported on this device.");
+        glfwTerminate();
+        return -1;
+    }
+
+    GLint maxTextureUnits;
+    glGetIntegerv(GL_MAX_TEXTURE_UNITS, &maxTextureUnits);
+    spdlog::debug("GL_MAX_TEXTURE_UNITS = {}", maxTextureUnits);
+
+    GLint maxVaryingFloats;
+    glGetIntegerv(GL_MAX_VARYING_FLOATS, &maxVaryingFloats);
+    spdlog::debug("GL_MAX_VARYING_FLOATS = {}", maxVaryingFloats);
 
     while (!glfwWindowShouldClose(window))
     {
