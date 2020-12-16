f02b12066dc5051a621487e138a74a0d2aab478e
Mehdi <gmcjeyzer@gmail.com>
2020-12-15 22:08:46 -0500

    pigment density variations section

--- report/code/wobbling-effect.glsl
+++ report/code/wobbling-effect.glsl
@@ -1,14 +1,10 @@
-vec2 getPaperGradient() {
-    float wobFactor = 0.05;
+vec2 getPaperGradient(vec3 tex) {
+    float wobF = 0.05;
 
-    float x_p = vertTexCoord
-                + vec2(wobFactor, 0);
-    float x_m = vertTexCoord
-                - vec2(wobFactor, 0);
-    float y_p = vertTexCoord
-                + vec2(0, wobFactor);
-    float y_m = vertTexCoord
-                - vec2(0, wobFactor);
+    float x_p = tex + vec2(wobF, 0);
+    float x_m = tex - vec2(wobF, 0);
+    float y_p = tex + vec2(0, wobF);
+    float y_m = tex - vec2(0, wobF);
 
     float dx = texture(paper, x_p).r
                 - texture(paper, x_m).r;
