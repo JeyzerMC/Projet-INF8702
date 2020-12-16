8f4dd783a3effb9a868affd68668d2f95daaa1c7
Mehdi <gmcjeyzer@gmail.com>
2020-12-15 22:51:02 -0500

    formatting and all

--- report/code/radial-darkening.glsl
+++ report/code/radial-darkening.glsl
@@ -1,6 +1,5 @@
 float vpDist = length(texCoords - 0.5);
 if (vpDist > 0.45) {
-   color *= 1 - remap(vpDist, 
-                      0.45, sqrt(0.5),
-                      0, 0.7);
+   color *= 1 - remap(vpDist, 0.45,
+                     sqrt(0.5), 0, 0.7);
 }
